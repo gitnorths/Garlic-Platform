b7e5465bd500150e75f654f80dccbcdba38bad8c
nanhua <gitnorth@sina.com>
2022-04-17 22:28:48 +0800

    初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
+  // 限制到当前目录下的项目
   root: true,
+  // 可用的环境变量
   env: {
-    node: true,
+    browser: true, // 浏览器环境中的全局变量
+    node: true, // Node.js 全局变量和 Node.js 作用域。
+    es6: true, // 启用除了 modules 以外的所有 ECMAScript 6 特性
   },
   extends: [
     "plugin:vue/essential",
@@ -10,6 +14,7 @@ module.exports = {
   ],
   parserOptions: {
     parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
