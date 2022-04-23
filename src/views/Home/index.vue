8e224fbef99125553e6ffee61a7dfabadd830d62
nanhua <gitnorth@sina.com>
2022-04-23 22:37:25 +0800

    feat: 增加数字滚动效果

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -21,23 +21,84 @@
     </el-header>
     <el-main>
       <div class="ds-flex">
-        <div class="left"></div>
-        <div class="center"></div>
-        <div class="right"></div>
+        <div class="ds-number left">
+          <ul>
+            <li>
+              <p>产量(吨）</p>
+              <countTo
+                :startVal="startVal"
+                :endVal="endVal"
+                :decimals="decimals"
+                :duration="duration"
+                class="color1"
+              ></countTo>
+            </li>
+            <li>
+              <p>产值(吨）</p>
+              <countTo
+                :startVal="startVal"
+                :endVal="endVal"
+                :decimals="decimals"
+                :duration="duration"
+                class="color2"
+              ></countTo>
+            </li>
+          </ul>
+        </div>
+        <div class="ds-map"></div>
+        <div class="ds-number right">
+          <ul>
+            <li>
+              <p>蒜头种植面积(km²)</p>
+              <countTo
+                :startVal="startVal"
+                :endVal="endVal"
+                :decimals="decimals"
+                :duration="duration"
+                class="color3"
+              ></countTo>
+            </li>
+            <li>
+              <p>蒜苗种植面积(km²)</p>
+              <countTo
+                :startVal="startVal"
+                :endVal="endVal"
+                :decimals="decimals"
+                :duration="duration"
+                class="color4"
+              ></countTo>
+            </li>
+            <li>
+              <p>蒜薹种植面积(km²)</p>
+              <countTo
+                :startVal="startVal"
+                :endVal="endVal"
+                :decimals="decimals"
+                :duration="duration"
+                class="color5"
+              ></countTo>
+            </li>
+          </ul>
+        </div>
       </div>
     </el-main>
   </el-container>
 </template>
 
 <script>
+import countTo from 'vue-count-to';
 export default {
   name: 'HomeView',
-  components: {},
+  components: { countTo },
   data() {
     return {
       setting: require('../../assets/images/home/setting.png'),
       message: require('../../assets/images/home/message.png'),
       user: require('../../assets/images/home/user.png'),
+      decimals: 1,
+      duration: 1500,
+      startVal: 0,
+      endVal: 12560.2,
     };
   },
 };
